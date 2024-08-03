99b0cbc66d8fd113a9df38afcbc32bd40df3b6fb
alitalipcoskun <ali_talip_18@hotmail.com>
2024-08-03 11:32:43 +0300

    dummy test

---
+++ __tests__/header.test.js
@@ -0,0 +1,29 @@
+import Header from '@/components/Header/Header'
+import { cn } from '@/lib/utils'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import { Inter as FontSans } from "next/font/google"
+
+const fontSans = FontSans({
+    subsets: ["latin"],
+    variable: "--font-sans",
+})
+
+const headerClass = cn(
+    "text-3xl font-bold underline",
+    fontSans.variable
+);
+
+
+describe("Header Component", () => {
+    it("Render the correct text in header component", () => {
+        render(<Header />)
+        const paragraphElement = screen.getByText("Deneme")
+        expect(paragraphElement).toBeInTheDocument();
+    });
+    it("Render the component with right class", () => {
+        render(<Header />)
+        const headerElement = screen.getByRole("heading");
+        expect(headerElement).toHaveClass(headerClass);
+    })
+});
