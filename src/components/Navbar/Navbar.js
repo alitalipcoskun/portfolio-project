110964acb4072088c996ca3f347bebc5aef9003b
alitalipcoskun <ali_talip_18@hotmail.com>
2024-08-03 18:36:30 +0300

    css fix

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -16,10 +16,9 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between px-4 py-2 bg-white dark:bg-gray-800">
         <Link href="" className="flex items-center gap-2" prefetch={false}>
-          <MountainIcon className="h-6 w-6" />
           <span className="text-lg font-semibold">Ali Talip Coşkun</span>
         </Link>
-        <div className="hidden md:flex gap-4">
+        <div className="hidden lg:flex gap-4">
           {
             NavItems.map((item, idx) => {
                 return <NavItem item= {item} key = {idx}/>
@@ -30,23 +29,6 @@ const Navbar = () => {
     </div>
   )
 }
-function MountainIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
-    </svg>
-  )
-}
+
 export default Navbar;
 
